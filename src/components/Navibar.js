e74e4b6d66493fd321da953cf877eae968ea5a63
Leonid <leoural66@yandex.ru>
2020-11-30 15:33:46 +0500

    работа с видео
    Добавил состояние кнопки ПРОСМОТР. Теперь она меняет название и стиль при просмотре видео и паузе. Меню сделал прозрачным.

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -6,12 +6,12 @@ import '../style/navibar.css';
 export default function Navibar() {
 
     return (
-        <Navbar collapseOnSelect expand="lg" bg="outline-dark" variant="light" className="fixed-top" style={{ backgroundColor: '#f8f6f6' }}>
+        <Navbar collapseOnSelect expand="lg" bg="outline-dark" variant="light" className="fixed-top navibar">
             <Container>
                 <Navbar.Brand href="#home" className="navibar_brand"><img src={logo} alt="logo" /> Boostfolia</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" style={{ position: 'relative' }}>
-                    <Nav className="mr-auto" style={{ position: 'absolute', right: '20px' }}>
+                    <Nav className="mr-auto" style={{ position: 'absolute', right: '20px', fontWeight: '500' }}>
                         <Nav.Link href="#about" className="mr-2">About us</Nav.Link>
                         <Nav.Link href="#works" className="mr-2">Our works</Nav.Link>
                         <Nav.Link href="#who" className="mr-2">Who we are</Nav.Link>
--- src/components/Watch.js
+++ src/components/Watch.js
@@ -4,44 +4,58 @@ import '../style/watch.css';
 import Videoblock from './Videoblock';
 
 
-export default function Watch() {
+export default class Watch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            playNow: 'PLAY NOW',
+            styleBtn: 'danger'
+        }
+        this.handlePlayVideo = this.handlePlayVideo.bind(this);
+    }
 
-    function handlePlayVideo() {
+
+    handlePlayVideo() {
         const videoPlay = document.getElementsByTagName('video')[0];
 
         if (videoPlay.paused) {
             videoPlay.play();
+            this.setState({ playNow: 'S T O P', styleBtn: 'warning' });
         } else {
             videoPlay.pause();
+            this.setState({ playNow: 'PLAY NOW', styleBtn: 'danger' });
         }
         console.log(videoPlay.paused);
     };
 
-    return (
-        <>
-            <Container fluid>
-                <Row>
-                    <Col style={{ padding: '0px', margin: '0' }}>
-                        <Card className="watch">
-                            <Card.Body>
-                                <Card.Title className="text-center watch_title">WATCH BEHIND THE SCENE</Card.Title>
-                                <Card.Text className="watch_text">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aliquid suscipit quasi voluptatem quas amet cum maiores ut quos ea, atque, quidem veritatis alias adipisci veniam iure error. Ducimus ratione impedit praesentium, eius libero error odio suscipit porro quia aperiam?
+    render() {
+        const playNow = this.state.playNow;
+        const styleBtn = this.state.styleBtn;
+        return (
+            <>
+                <Container fluid>
+                    <Row>
+                        <Col style={{ padding: '0px', margin: '0' }}>
+                            <Card className="watch">
+                                <Card.Body>
+                                    <Card.Title className="text-center watch_title">WATCH BEHIND THE SCENE</Card.Title>
+                                    <Card.Text className="watch_text">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aliquid suscipit quasi voluptatem quas amet cum maiores ut quos ea, atque, quidem veritatis alias adipisci veniam iure error. Ducimus ratione impedit praesentium, eius libero error odio suscipit porro quia aperiam?
                                 </Card.Text>
-                                <Button variant="danger" className="watch_btn" onClick={handlePlayVideo} >PLAY NOW</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col style={{ padding: '0px', margin: '0' }}>
-                        <Card className="video_block">
-                            <Card.Body style={{ padding: '0px' }}>
-                                <Videoblock />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
-
+                                    <Button variant={styleBtn} className="watch_btn" onClick={this.handlePlayVideo}>{playNow}</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col style={{ padding: '0px', margin: '0' }}>
+                            <Card className="video_block">
+                                <Card.Body style={{ padding: '0px' }}>
+                                    <Videoblock />
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 }
